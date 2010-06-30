03effb6293ac321c6a0a4d78e8f51404daf7b4ec
Rajib Ahmed <l.rajibahmed@gmail.com>
2010-06-30 16:34:38 +0600

    git ignore added

---
+++ config/sql/schema.php
@@ -0,0 +1,20 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* D32SUMS schema generated on: 2010-06-30 09:06:40 : 1277891920*/
+class D32SUMSSchema extends CakeSchema {
+	var $name = 'D32SUMS';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+	
+	var $teachers =array(
+		
+		
+	);
+
+}
+?>
